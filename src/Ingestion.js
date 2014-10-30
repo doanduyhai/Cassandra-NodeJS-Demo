80646d708ccae0ff729ff108a6e92869ca2f1397
DuyHai DOAN <doanduyhai@gmail.com>
2014-10-30 15:08:53 +0100

    Add async package to guarantee ordering

--- src/Ingestion.js
+++ src/Ingestion.js
@@ -2,13 +2,10 @@ var fs = require('fs');
 var parse = require('csv-parse');
 var transform = require('stream-transform');
 var cassandra = require('cassandra-driver');
+var async = require('async');
 
-var parser = parse({delimiter: ','})
-var input = fs.createReadStream('data/us_unemployment.csv');
 
 var client = new cassandra.Client({contactPoints: ['localhost']});
-
-initCassandraSchema(client)
 
 var insert = "INSERT INTO nodejs_demo.us_unemployment ( \
         year, \
@@ -26,39 +23,52 @@ var insert = "INSERT INTO nodejs_demo.us_unemployment ( \
         VALUES(?,?,?,?,?,?,?,?,?,?,?,?)";
 
 
+async.series([
+    function(next){initCassandraSchema(client, next)},
+    function(next){
+        var parser = parse({delimiter: ','})
+        var input = fs.createReadStream('data/us_unemployment.csv');
 
-var transformer = transform(function(line){
-    client.execute(insert, parseData(line), {prepare:true, consistency:cassandra.types.consistencies.one},displayError);
-    return line;
-});
+        var transformer = transform(function(line){
+            client.execute(insert, parseData(line), {prepare:true, consistency:cassandra.types.consistencies.one},next);
+            return line;
+        });
 
-transformer.on('finish', function(){
-    console.log("Finish importing data into Cassandra")
-    process.exit()
-});
+        transformer.on('error', next);
 
-input.pipe(parser).pipe(transformer);
+        input.pipe(parser).pipe(transformer);
+    }
+],displayError);
 
 
-function initCassandraSchema(client) {
+
+function initCassandraSchema(client, next) {
     console.log("********** Initializing schema for demo **********")
-    client.execute("CREATE KEYSPACE IF NOT EXISTS nodejs_demo WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }", displayError);
 
-    client.execute("CREATE TABLE IF NOT EXISTS nodejs_demo.us_unemployment ( \
-        year int PRIMARY KEY, \
-        civil_non_institutional_count int, \
-        civil_labor_count int, \
-        labor_population_percentage double, \
-        employed_count int, \
-        employed_percentage double, \
-        agriculture_part_count int, \
-        non_agriculture_part_count int, \
-        unemployed_count int, \
-        unemployed_percentage_to_labor double, \
-        not_labor_count int,\
-        footnotes text)",displayError);
 
-    client.execute("TRUNCATE nodejs_demo.us_unemployment", displayError)
+    async.series([
+        function(nextCall) {
+            client.execute("CREATE KEYSPACE IF NOT EXISTS nodejs_demo WITH REPLICATION = { 'class' : 'SimpleStrategy', 'replication_factor' : 1 }",[], nextCall);
+        },
+        function(nextCall) {
+            client.execute("CREATE TABLE IF NOT EXISTS nodejs_demo.us_unemployment ( \
+                year int PRIMARY KEY, \
+                civil_non_institutional_count int, \
+                civil_labor_count int, \
+                labor_population_percentage double, \
+                employed_count int, \
+                employed_percentage double, \
+                agriculture_part_count int, \
+                non_agriculture_part_count int, \
+                unemployed_count int, \
+                unemployed_percentage_to_labor double, \
+                not_labor_count int,\
+                footnotes text)",[],nextCall);
+        },
+        function(nextCall) {
+            client.execute("TRUNCATE nodejs_demo.us_unemployment", [],nextCall)
+        }
+    ], next);
 }
 
 function parseData(line) {
